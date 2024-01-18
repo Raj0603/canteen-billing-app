faf5baa333e743c254989adbd9a184192a880ba8
Raj0603 <rajsrivastav0603@gmail.com>
2024-01-18 23:57:39 +0530

    student side completed

--- frontend/src/components/Student/Orders/MyOrder.jsx
+++ frontend/src/components/Student/Orders/MyOrder.jsx
@@ -1,26 +1,40 @@
-import React from 'react'
-import "./MyOrder.css"
-import { useSelector, useDispatch } from "react-redux";
-import { useEffect, useState } from "react";
-import { myOrders } from "../../../actions/orderAction";
-import { useAlert } from "react-alert";
+import React from 'react';
+import './MyOrder.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { myOrders } from '../../../actions/orderAction';
+import { useAlert } from 'react-alert';
 import OrderCard from './OrderCard';
+import Loading from '../../Loading/Loading';
 
 export const MyOrder = () => {
+  const alert = useAlert();
+  const dispatch = useDispatch();
+  const { orders, loading, error } = useSelector((state) => state.myOrders);
 
-  // const alert = useAlert();
-  // const dispatch = useDispatch();
-  // const { order } = useSelector((state) => state.myOrders);
+  useEffect(() => {
+    if (error) {
+      alert.error(error);
+      dispatch(clearErrors());
+    }
 
-  // useEffect(() => {
-    
-  //   dispatch(myOrders())
-  // },[dispatch])
-  
+    dispatch(myOrders());
+  }, [dispatch, alert, error]);
 
   return (
-    <div className='my-mc'>
-      
-    </div>
-  )
-}
+    <>
+      {loading ? (
+        <Loading />
+      ) : (
+        <div className='my-mc'>
+          <h2>Order History</h2>
+          {orders &&
+            orders
+              .slice() // Create a shallow copy to avoid mutating the original array
+              .reverse() // Reverse the array
+              .map((order) => <OrderCard order={order} key={order.paidAt} />)}
+        </div>
+      )}
+    </>
+  );
+};
--- frontend/src/components/Student/Orders/OrderCard.jsx
+++ frontend/src/components/Student/Orders/OrderCard.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import vegImg from "../../../assets/iv.png";
 import nonVegImg from "../../../assets/inv.png";
+import "./MyOrder.css"
+import osimg from "../../../assets/osimg.jpg"
 
-const OrderCard = ({ order }) => {
+const OrderCard = ({ order}) => {
 
   const timestamp = order.paidAt;
   const formattedDate = new Date(timestamp).toLocaleDateString("en-GB", {
@@ -11,25 +13,37 @@ const OrderCard = ({ order }) => {
     year: "numeric",
   });
 
-  let icon = "";
-
-  if (item.type == "Veg") {
-    icon = <img src={vegImg} alt="" />;
-  } else {
-    icon = <img src={nonVegImg} alt="" />;
-  }
   return (
     <div className="oc-mc">
       <div className="oc-uc">
-        <span>Status:{order.orderStatus}</span>
+        {/* <img src={osimg} alt="" className="oc-osi" /> */}
+
+        <h3>Status  {order.orderStatus}</h3>
       </div>
-      <div className="oc-ld">
-        <span>Items</span>
-        <span>
-          {order.orderItems.type} {order.orderItems.quantity} X{" "}
-          {order.orderItems.name}
-        </span>
-        <span>{formattedDate} {order.totalPrice}</span>
+      <div className="oc-lc">
+        <span style={{color:"#696999"}}>Items</span>
+        {/* {order.orderItems.map((item) => (
+          <span key={item._id}>
+            {icon} {item.quantity} X {item.name}
+          </span>
+        ))} */}
+        {order.orderItems.map((item) => (
+          <div key={item._id} className="oc-item">
+            {item.type === "Veg" ? (
+              <img src={vegImg} alt="Veg" />
+            ) : (
+              <img src={nonVegImg} alt="Non-Veg" />
+            )}
+            <span>
+              {item.quantity} X {item.name}
+            </span>
+          </div>
+        ))}
+        <span className="oc-bl"></span>
+        <div>
+          
+        <span style={{color:"#696999"}}>{formattedDate}</span> <span style={{marginLeft:"55vw"}}> ₹ {order.totalPrice}</span>
+        </div>
       </div>
     </div>
   );
